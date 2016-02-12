66d5deb3b463e29f5a5a6c189f836dbe2d5f96a6
Garry Oborn <Garry Oborn>
2016-02-12 16:58:11 -0700

    Conditional statement.java has been modified

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -3,7 +3,7 @@ public class ConditionalStatement {
 
 	public static void main(String[] args) {
 		
-		int score = 91;
+		int score = 30;
 		String grade;
 		
 		if (score > 90) {
@@ -12,9 +12,18 @@ public class ConditionalStatement {
 		else if (score > 80){
 			grade = "B";
 		}
-		else {
+		else if (score > 70) {
 			grade = "C";
 		}
+		else if (score > 40) {
+			grade = "D-";
+			System.out.println("Please give up now because you got an " + grade);
+		}
+		else {
+			grade = "F";
+			System.out.println("You suck rocks because you received an " + grade);
+		}
+		
 		System.out.println("Score is: " + score);
 		System.out.println("Score is: " + grade);
 		
