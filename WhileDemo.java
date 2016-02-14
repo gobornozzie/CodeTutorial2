9a6e6070ecb06ee75b1371de3fffd9f8e3b66624
Garry <gtoborn@gmail.com>
2016-02-14 11:31:35 -0700

    while demo modified

--- WhileDemo.java
+++ WhileDemo.java
@@ -3,27 +3,32 @@ public class WhileDemo {
 
 	public static void main(String[] args) {
 		
-		int i = 10;
+		int i = 5;
 		
-		while (i < 10) {
+		while (i < 20 ) {
 			System.out.println(i);
 			i++;
-			if(i==17) {
+			if(i == 17) {
 				continue;
 			}
 			System.out.println("Value of i" + i);
-			
 		}
-		System.out.println("Outside loop...");
-	
-		do {
+		System.out.println("Outside Loop");
+		
+		do{
 			System.out.println("Do while loop...");
 			System.out.println(i);
-			i++;			
+			i++;
 			
-		} while (i < 10);
-	
-	
+		}while (i < 23);
+		
+		if (i == 23){
+			System.out.println("if statement executed");
+		}
+		else{
+			System.out.println("else statement executed");
+		}
+			
 	}
 
 }
